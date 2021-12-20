c9546234fd353482913b9d255eea9b52938f41b1
yusaku.kuwahata <xyusaku.kuwahata@gmail.com>
2021-12-20 06:54:57 +0000

    add view tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+    
+    def show
+        @task = Task.find(params[:id])
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new(task_params)
+        
+        if @task.save
+            flash[:success] = 'タスクの作成に成功しました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'タスクの作成に失敗しました'
+            render :new
+        end
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def update
+        @task = Task.find(params[:id])
+        
+        if @task.update(task_params)
+            flash[:success] = 'タスクの更新に成功しました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'タスクの更新に失敗しました'
+            render :edit
+        end
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = 'タスクの更新に成功しました'
+        redirect_to tasks_url
+    end
+    
+    private
+    
+    def task_params 
+        params.require(:task).permit(:content)
+    end
+end
